0c6c690c5e874f4dc386a0f18e7da362fd91fa24
Alejandro Arnaiz <aarnaiz@habitissimo.com>
2015-11-27 10:21:19 +0100

    Charset utf8 and database credentials in environment variable

--- utils/Database.php
+++ utils/Database.php
@@ -8,14 +8,23 @@
  */
 class DataBase
 {
-    const USER = 'root';
-    const PASSWORD = 'root';
+    const
+        DB_USER = 'DB_USER',
+        DB_PWD = 'DB_PWD',
+        DB_NAME = 'DB_NAME',
+        DB_HOST = 'DB_HOST';
+
+
     private static $connection = null;
 
     public static function connect()
     {
         if (!self::$connection) {
-            self::$connection = new PDO('mysql:host=localhost;dbname=ping-pong', self::USER, self::PASSWORD);
+            $db_host = getenv(self::DB_HOST);
+            $db_user = getenv(self::DB_USER);
+            $db_pwd = getenv(self::DB_PWD);
+            $db_name = getenv(self::DB_NAME);
+            self::$connection = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", $db_user, $db_pwd);
         }
         return self::$connection;
     }
